5b2656ea507ad8895cbf4d6561c81d2d2022fc23
Bhawna Goswami <bhawna.goswami2@synapse.com>
2020-11-03 12:52:01 +0530

    working get and post

--- document-data/src/main/resources/data.sql
+++ document-data/src/main/resources/data.sql
@@ -1,14 +1,9 @@
-DROP TABLE IF EXISTS documentData
 
-CREATE TABLE documentData(
-docid varchar(10),
-docname varchar(30),
-doclocation varchar(100));
 
-INSERT INTO documentData
+INSERT INTO DOCUMENTDATA
 (docid, docname, doclocation)
 VALUES('1','Aadhar','my D drive');
 
-INSERT INTO documentData
+INSERT INTO DOCUMENTDATA
 (docid, docname, doclocation)
 VALUES('2','PAN','HardDisk');
---
+++ document-data/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS DOCUMENTDATA;
+
+CREATE TABLE DOCUMENTDATA(
+docid varchar(10) PRIMARY KEY,
+docname varchar(30),
+doclocation varchar(100));
