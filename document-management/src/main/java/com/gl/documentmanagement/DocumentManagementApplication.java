24c7247917d8eb4f249e0517c5a9036368cbb2c7
bhawnagoswami-gl <bhawna.goswami2@globallogic.com>
2020-11-11 14:33:15 +0530

    reference code

--- document-management/src/main/java/com/gl/documentmanagement/DocumentManagementApplication.java
+++ document-management/src/main/java/com/gl/documentmanagement/DocumentManagementApplication.java
@@ -3,7 +3,6 @@ package com.gl.documentmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
